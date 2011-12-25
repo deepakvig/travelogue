5cd49acb9d042118428247da271eb26897b03338
Deepak Vig <deepakvig@gmail.com>
2011-12-25 14:22:09 +0530

    Login Articles updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  load_and_authorize_resource
+
   # GET /articles
   # GET /articles.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Travelogue::Application.routes.draw do
 
   devise_for :users
 
+  root :to => 'articles#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
