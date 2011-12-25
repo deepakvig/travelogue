eed48386833a75638aa8138142f2c3219eeb4c5b
Deepak Vig <deepakvig@gmail.com>
2011-12-25 13:28:02 +0530

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelogue::Application.config.secret_token = 'f21394bd167814e52119f79e57773de9614f0c62e81aeb60ffb22f6f37714dbb9c9f61ddae4caf2d19f4eb6a006486eb7a7e340e9acc7c1f5e4a4e0df3342b75'
