87a343bc32d969015df13659682097f6259ec240
Deepak Vig <deepakvig@gmail.com>
2011-12-27 08:51:37 +0530

    Tagging added

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,14 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
   has_and_belongs_to_many :categories
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   validates :title, :content, :presence => true
+
+  has_many :taggings, :dependent => :destroy
+  has_many :tags, :through => :taggings
+
+  attr_writer :tag_names
+  after_save :assign_tags
  
   scope :published, where("articles.published_at IS NOT NULL")
   scope :draft, where("articles.published_at IS NULL")
@@ -17,4 +23,17 @@ class Article < ActiveRecord::Base
   def published?
     published_at.present?
   end
+
+  def tag_names
+    @tag_names || tags.map(&:tag_name).join(' ')
+  end
+ 
+  private
+    def assign_tags
+      if @tag_names
+        self.tags = @tag_names.split(/\s+/).map do |name|
+          Tag.find_or_create_by_tag_name(name)
+        end
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111225171026) do
+ActiveRecord::Schema.define(:version => 20111227030944) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -60,6 +60,22 @@ ActiveRecord::Schema.define(:version => 20111225171026) do
   end
 
   add_index "comments", ["article_id"], :name => "index_comments_on_article_id"
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "article_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["article_id"], :name => "index_taggings_on_article_id"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+
+  create_table "tags", :force => true do |t|
+    t.string   "tag_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
